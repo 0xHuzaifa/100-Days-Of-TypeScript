b928dc19f3a9a1308e2735b5c49e7281972808dc
huzaafi <huzaifasaleem098@gmail.com>
2024-03-21 23:53:26 +0500

    upload

---
+++ Day_20/task01.js
@@ -0,0 +1,12 @@
+/*
+* Question 58: Average Score Calculator: Write a simple program that can take lots of scores and find their average.
+
+* Explain & TIP: This program can handle any number of scores you give it,
+* and it tells you the average score. Handy for seeing how well you did overall!
+*/
+function averageScore(...scores) {
+    const num = scores.reduce((total, score) => total + score, 0) / scores.length;
+    return num;
+}
+console.log("Average Number", averageScore(1, 2, 3, 4, 5, 9, 10, 15, 35, 50, 80, 20, 50, 65, 45, 100, 150, 33, 22, 41));
+export {};
---
+++ Day_20/task02.ts
@@ -0,0 +1,24 @@
+/*
+* Question 59: Add a Special Number: Make a program that creates custom adders. 
+* These adders can add a specific number to any other number you give them later.
+
+* Explain & TIP: Imagine you have a magic box that can add a specific number to any number you put in it. 
+* This program makes that magic box for you!
+*/
+
+class customNumber {
+    private readonly specificNumber;
+
+    constructor(specificNumber: number) {
+        this.specificNumber = specificNumber;
+    }
+
+    add(input: number) {
+        return this.specificNumber + input;    
+    }
+}
+
+const magicBox = new customNumber(5); // Create a custom adder with a special number of 5
+console.log(magicBox.add(5)); // Output: 10 (5 + 5)
+console.log(magicBox.add(9)); // Output: 14 (9 + 5)
+
